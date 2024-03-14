0c5520a1091bf76ec00fefa5d12faa7b5788b4f0
shafkat63 <msm.shafkat99@gmail.com>
2024-03-14 17:06:54 +0600

    Added Database and authentication

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,12 +1,18 @@
 "use client";
+import { useConvexAuth } from "convex/react";
 
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { ModeToggle } from "@/components/mode-toggle";
 
 import Logo from "./logo";
+import { SignInButton,UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import Spinner from "@/components/spinner";
+import Link from "next/link";
 
 const Navbar = () => {
+	const { isAuthenticated, isLoading } = useConvexAuth();
 	const scrolled = useScrollTop();
 	return (
 		<div
@@ -20,6 +26,29 @@ const Navbar = () => {
 				className="md:ml-auto md:justify-end justify-between w-full 
 			flex items-center gap-x-2"
 			>
+				{isLoading && <Spinner />}
+				{!isAuthenticated && !isLoading && (
+					<>
+						<SignInButton mode="modal">
+							<Button variant="ghost" size="sm">
+								Log in
+							</Button>
+						</SignInButton>
+						<SignInButton mode="modal">
+							<Button size="sm">Get Botion Free </Button>
+						</SignInButton>
+					</>
+				)}
+				{isAuthenticated && !isLoading && (
+					<>
+						<Button variant="ghost" size="sm">
+							<Link href="/documents">Enter Botion</Link>
+						</Button>
+						<UserButton afterSignOutUrl="/">
+
+						</UserButton>
+					</>
+				)}
 				<ModeToggle />
 			</div>
 		</div>
